55ca5b8aff7e53570dc0eec862af07d8056686e4
Adam Beardow <abeardow13@gmail.com>
2023-01-04 22:50:30 +0000

    Added a sign in page

--- php/layout.php
+++ php/layout.php
@@ -3,21 +3,21 @@
     function createHeader(){
         $html = 
         '<div class="topbar">
-            <h1 class="title">Selby Bookstore</h1>
-            </div>
-            <div class="navbar">
-                <nav>
-                    <a href="index.php"><h1>Home</h1></a>
-                </nav>
-                <nav>
-                    <a href="account.php"><h1>Account</h1></a>
-                </nav>
-                <nav>
-                    <a href="cart.php"><h1>Cart</h1></a>
-                </nav>
-                <nav>
-                    <a href="about.php"><h1>About</h1></a>
-                </nav>
+            <h1 id="title">Selby Bookstore</h1>
+        </div>
+        <div class="navbar">
+            <nav>
+                <a href="index.php"><h1>Home</h1></a>
+            </nav>
+            <nav>
+                <a href="account.php"><h1>Account</h1></a>
+            </nav>
+            <nav>
+                <a href="cart.php"><h1>Cart</h1></a>
+            </nav>
+            <nav>
+                <a href="about.php"><h1>About</h1></a>
+            </nav>
         </div>';
         echo $html;
     }

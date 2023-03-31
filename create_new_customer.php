886c860c964bc0960b0483777c7ad02584bcc80e
Adam Beardow <abeardow13@gmail.com>
2023-03-31 19:40:38 +0100

    Fixed sign in button still being visible after creating an account

--- create_new_customer.php
+++ create_new_customer.php
@@ -16,64 +16,67 @@
 
         createHeader();
 
-        $email = $_POST["email"];
-        $title = $_POST["title"];
-        $fname = $_POST["fname"];
-        $sname = $_POST["sname"];
-        $dob = $_POST["dob"];
-        $houseNum = $_POST["houseNum"];
-        $street = $_POST["street"];
-        $town = $_POST["town"];
-        $county = $_POST["county"];
-        $country = $_POST["country"];
-        $postCode = $_POST["postCode"];
-        $password = $_POST["password"];
-        $confirmPassword = $_POST["confirmPassword"];
-
-        if ($password === $confirmPassword)
+        if (!isset($_GET["details_valid"]))
         {
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $email = $_POST["email"];
+            $title = $_POST["title"];
+            $fname = $_POST["fname"];
+            $sname = $_POST["sname"];
+            $dob = $_POST["dob"];
+            $houseNum = $_POST["houseNum"];
+            $street = $_POST["street"];
+            $town = $_POST["town"];
+            $county = $_POST["county"];
+            $country = $_POST["country"];
+            $postCode = $_POST["postCode"];
+            $password = $_POST["password"];
+            $confirmPassword = $_POST["confirmPassword"];
 
-            $conn = openConnection();
+            if ($password === $confirmPassword)
+            {
+                $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-            $sql = 
-            "
-            INSERT INTO Bookstore.Customers(Email, Title, FirstName, Surname, DateOfBirth, HouseNumber, Street, Town, County, Country, PostCode, Password)
-            VALUES ('$email', '$title', '$fname', '$sname', '$dob', '$houseNum', '$street', '$town', '$county', '$country', '$postCode', '$hashedPassword')
-            ";
-    
-            echo "<h2 class='content'>";
-    
-            if ($conn->query($sql) === TRUE)
-                echo "Account created successfully";
+                $conn = openConnection();
+
+                $sql = 
+                "
+                INSERT INTO Bookstore.Customers(Email, Title, FirstName, Surname, DateOfBirth, HouseNumber, Street, Town, County, Country, PostCode, Password)
+                VALUES ('$email', '$title', '$fname', '$sname', '$dob', '$houseNum', '$street', '$town', '$county', '$country', '$postCode', '$hashedPassword')
+                ";
+
+                $customer = new Customer();
+                $customer->Email = $email;
+                $customer->FirstName = $fname;
+                $customer->Surname = $sname;
+                $customer->DateOfBirth = $dob;
+                $customer->HouseNumber = $houseNum;
+                $customer->Street = $street;
+                $customer->Town = $town;
+                $customer->County = $county;
+                $customer->Country = $country;
+                $customer->PostCode = $postCode;
+                $customer->Password = $hashedPassword;
+
+                $_SESSION["Customer"] = $customer;
+                
+                $conn->close();
+
+                header('Location: create_new_customer.php?details_valid=true');
+            }
             else
-                echo "Account could not be created: " . $conn->error;
-    
-            echo "</h2>";
-
-            $customer = new Customer();
-            $customer->Email = $email;
-            $customer->FirstName = $fname;
-            $customer->Surname = $sname;
-            $customer->DateOfBirth = $dob;
-            $customer->HouseNumber = $houseNum;
-            $customer->Street = $street;
-            $customer->Town = $town;
-            $customer->County = $county;
-            $customer->Country = $country;
-            $customer->PostCode = $postCode;
-            $customer->Password = $hashedPassword;
-
-            $_SESSION["Customer"] = $customer;
-            
-            $conn->close();
+            {
+                echo "<h2 class='content'>";
+                echo "Passwords do not match!";
+                echo "</h2>";
+            }
         }
+
         else
-        {
+        {                   
             echo "<h2 class='content'>";
-            echo "Passwords do not match!";
+            echo "Account created successfully";    
             echo "</h2>";
-        }
+        }        
 
     ?>
 </body>
